52c8b58a1d1ab24d6919f7fc075d90de6c51b2c8
o.starostin <starostinoleg370@gmail.com>
2015-02-19 20:13:16 +0200

    Fixed scroll

--- www/source/views/main_screen/news_list/news_list.js
+++ www/source/views/main_screen/news_list/news_list.js
@@ -1,7 +1,38 @@
-RAD.view("view.news_list", RAD.Blanks.ScrollableView.extend({
+RAD.view("view.news_list", RAD.Blanks.View.extend({
     url: 'source/views/main_screen/news_list/news_list.html',
-    scrollOptions: {
-        useTransition: true,
-        topOffset: 50
+    onattach: function(){
+        var self = this,
+            wrapper = this.el.querySelector('.scroll-view');
+        this.mScroll = new iScroll(wrapper, {
+            useTransition: true,
+            hScrollbar: false,
+            vScrollbar: false,
+            topOffset: 50,
+            y: -50,
+            onRefresh: function(){
+                self.onScrollRefresh();
+            },
+            onScrollMove: function(){
+                self.onScrollMove();
+            },
+            onScrollEnd: function(){
+                self.onScrollEnd();
+            }
+        })
+    },
+    ondetach: function(){
+        if (this.mScroll) {
+            this.mScroll.destroy();
+        }
+        this.mScroll = null;
+    },
+    onScrollRefresh: function(){
+        console.log('-----------------REFRESH---------------')
+    },
+    onScrollMove: function(){
+        console.log('-----------------MOVE---------------')
+    },
+    onScrollEnd: function(){
+        console.log('-----------------END---------------')
     }
 }));
