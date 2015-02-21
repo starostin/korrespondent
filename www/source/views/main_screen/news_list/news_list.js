7f60a6606e5bf334598ba04a50f68cf0daecfc7c
o.starostin <starostinoleg370@gmail.com>
2015-02-21 18:27:53 +0200

    Fixed scroll

--- www/source/views/main_screen/news_list/news_list.js
+++ www/source/views/main_screen/news_list/news_list.js
@@ -12,8 +12,9 @@ RAD.view("view.news_list", RAD.Blanks.View.extend({
             onRefresh: function(){
                 self.onScrollRefresh();
             },
-            onScrollMove: function(){
-                self.onScrollMove();
+            onScrollMove: function(e){
+                //console.log(e)
+                self.onScrollMove(e);
             },
             onScrollEnd: function(){
                 self.onScrollEnd();
@@ -26,13 +27,61 @@ RAD.view("view.news_list", RAD.Blanks.View.extend({
         }
         this.mScroll = null;
     },
+    onInitialize: function(){
+        this.rotateCoef = 180/50;
+    },
     onScrollRefresh: function(){
         console.log('-----------------REFRESH---------------')
     },
-    onScrollMove: function(){
-        console.log('-----------------MOVE---------------')
+    onScrollMove: function(e){
+        console.log('---------------MOVE-----------')
+        if(this.mScroll.y<-50){
+            return;
+        }
+        var pullDiv = this.el.querySelector('.pull-down'),
+            arrow = pullDiv.querySelector('.arrow-img'),
+            deg = Math.abs(this.rotateCoef * this.mScroll.y)-180;
+        if(this.mScroll.y>0){
+            deg = -180;
+        }
+
+        arrow.style.transform = 'rotate(' + deg + 'deg)';
+        if(deg <= -180 && !pullDiv.classList.contains('update')){
+            console.log('-=-=-=-=-=-=UPDATE-=-=-=-=', this.mScroll.maxScrollY)
+            this.mScroll.minScrollY = 0;
+            pullDiv.classList.add('update');
+        }else if(!pullDiv.classList.contains('update')){
+            console.log('-=-=-=-=-=-=NOT-=-=-=-=')
+            this.mScroll.minScrollY = -50;
+            pullDiv.classList.remove('update');
+        }else{
+            console.log('-=-=-=-=-=-=ASSSSSSS-=-=-=-=')
+        }
+    },
+    getNews: function(){
+        var self = this,
+            pullDiv = this.el.querySelector('.pull-down'),
+            arrow = pullDiv.querySelector('.arrow-img'),
+            spinner = pullDiv.querySelector('.loader');
+        arrow.style.display = 'none';
+        spinner.style.display = '';
+        window.setTimeout(function(){
+            spinner.style.display = 'none';
+            pullDiv.classList.remove('update');
+            self.mScroll.refresh();
+            arrow.style.display = '';
+        }, 1000)
     },
     onScrollEnd: function(){
+        var pullDiv = this.el.querySelector('.pull-down'),
+            arrow = pullDiv.querySelector('.arrow-img'),
+            isUpdate = pullDiv.classList.contains('update'),
+            spinner = pullDiv.querySelector('.loader');
+        if(isUpdate && spinner.style.display === 'none'){
+            console.log('--------------GET NEWS---------------')
+            this.getNews();
+        }
+
         console.log('-----------------END---------------')
     }
 }));
