d72fc5a46b9c12948da228766d76792097f41053
o.starostin <starostinoleg370@gmail.com>
2015-05-01 17:56:22 +0300

    Fixed parallax

--- www/settings.js
+++ www/settings.js
@@ -6,4 +6,5 @@
     settings.version = '1.0';
     //cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-device.git
     //cordova plugin add de.appplant.cordova.plugin.email-composer@0.8.2
+    //cordova plugin add nl.x-services.plugins.socialsharing
 })(this);
