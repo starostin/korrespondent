339c4000be7be2d3aa62a7fdd34d9723571a75f6
starostin <starostinoleg89@mail.ru>
2015-11-01 23:15:52 +0200

    Add menu data

--- www/source/models/menu_data.js
+++ www/source/models/menu_data.js
@@ -19,39 +19,62 @@ RAD.newsUrls = {
     9: 'football',
     10: 'basketball',
     11: 'events',
-    12: 'politics'
+    12: 'politics',
+    13: 'tenis',
+    14: 'formula',
+    15: 'hokey',
+    16: 'chess',
+    17: 'business',
+    18: 'companies',
+    19: 'economics',
+    20: 'financial',
+    21: 'mmedia_and_adv',
+    22: 'auto',
+    23: 'indexes',
+    24: 'rynki',
+    25: 'web',
+    26: 'realestate',
+    27: 'career',
+    28: 'tech',
+    29: 'technews',
+    30: 'science',
+    31: 'space',
+    32: 'world',
+    33: 'worldabus',
+    34: 'russia',
+    35: 'lifestyle',
+    36: 'afisha',
+    37: 'fashion',
+    38: 'travel',
+    39: 'fooddrinks',
+    40: 'motors',
+    41: 'gadgets',
+    42: 'interiors',
+    43: 'health',
+    44: 'videoigry',
+    45: 'good_news',
+    46: 'kyiv',
+    47: 'mainbyday'
 };
 RAD.menuMapping = {
     rus: [
         {
-            id: 1,
-            title: 'Щоу-бизнес',
+            id: 3,
+            title: 'Все новости',
             subMenus: [
                 {
-                    id: 1,
-                    title: 'Щоу-бизнес'
-                },
-                {
-                    id: 5,
-                    title: 'новости культуры'
-                },
-                {
-                    id: 6,
-                    title: 'новости кино'
-                },
-                {
-                    id: 7,
-                    title: 'музыка'
+                    id: 3,
+                    title: 'Все новости'
                 }
             ]
         },
         {
             id: 2,
-            title: 'Спорт',
+            title: 'Новости спорта',
             subMenus: [
                 {
                     id: 2,
-                    title: 'Спорт'
+                    title: 'Новости спорта'
                 },
                 {
                     id: 8,
@@ -64,16 +87,94 @@ RAD.menuMapping = {
                 {
                     id: 10,
                     title: 'Баскетбол'
+                },
+                {
+                    id: 13,
+                    title: 'Теннис'
+                },
+                {
+                    id: 14,
+                    title: 'Формула 1'
+                },
+                {
+                    id: 15,
+                    title: 'Хоккей'
+                },
+                {
+                    id: 16,
+                    title: 'Шахматы'
                 }
             ]
         },
         {
-            id: 3,
-            title: 'Все новости',
+            id: 17,
+            title: 'Новости бизнеса',
             subMenus: [
                 {
-                    id: 3,
-                    title: 'Все новости'
+                    id: 17,
+                    title: 'Новости бизнеса'
+                },
+                {
+                    id: 18,
+                    title: 'Новости компаний'
+                },
+                {
+                    id: 19,
+                    title: 'Новости экономики'
+                },
+                {
+                    id: 20,
+                    title: 'Новости финансов'
+                },
+                {
+                    id: 21,
+                    title: 'СМИ и реклама'
+                },
+                {
+                    id: 22,
+                    title: 'Авто новости'
+                },
+                {
+                    id: 23,
+                    title: 'Индексы'
+                },
+                {
+                    id: 24,
+                    title: 'Фондовый рынок'
+                },
+                {
+                    id: 25,
+                    title: 'Интернет и IT'
+                },
+                {
+                    id: 26,
+                    title: 'Недвижимость'
+                },
+                {
+                    id: 27,
+                    title: 'Карьера'
+                }
+            ]
+        },
+        {
+            id: 1,
+            title: 'Шоу-бизнес',
+            subMenus: [
+                {
+                    id: 1,
+                    title: 'Шоу-бизнес'
+                },
+                {
+                    id: 5,
+                    title: 'Новости культуры'
+                },
+                {
+                    id: 6,
+                    title: 'Новости кино'
+                },
+                {
+                    id: 7,
+                    title: 'Музыка'
                 }
             ]
         },
@@ -92,6 +193,122 @@ RAD.menuMapping = {
                 {
                     id: 12,
                     title: 'Новости политики'
+                }
+            ]
+        },
+        {
+            id: 28,
+            title: 'Наука и медицина',
+            subMenus: [
+                {
+                    id: 28,
+                    title: 'Наука и медицина'
+                },
+                {
+                    id: 29,
+                    title: 'Технологии'
+                },
+                {
+                    id: 30,
+                    title: 'Новости науки'
+                },
+                {
+                    id: 31,
+                    title: 'Новости космоса'
+                }
+            ]
+        },
+        {
+            id: 32,
+            title: 'Новости мира',
+            subMenus: [
+                {
+                    id: 32,
+                    title: 'Новости мира'
+                },
+                {
+                    id: 33,
+                    title: 'Мир о нас'
+                },
+                {
+                    id: 34,
+                    title: 'Новости России'
+                }
+            ]
+        },
+        {
+            id: 35,
+            title: 'Lifestyle',
+            subMenus: [
+                {
+                    id: 35,
+                    title: 'Lifestyle'
+                },
+                {
+                    id: 36,
+                    title: 'Афиша мероприятий'
+                },
+                {
+                    id: 37,
+                    title: 'Новости моды'
+                },
+                {
+                    id: 38,
+                    title: 'Новости туризма'
+                },
+                {
+                    id: 39,
+                    title: 'Еда и напитки'
+                },
+                {
+                    id: 40,
+                    title: 'Автомобили'
+                },
+                {
+                    id: 41,
+                    title: 'Гаджеты'
+                },
+                {
+                    id: 42,
+                    title: 'Интерьеры'
+                },
+                {
+                    id: 43,
+                    title: 'Здоровье'
+                },
+                {
+                    id: 44,
+                    title: 'Видеоигры'
+                }
+            ]
+        },
+        {
+            id: 45,
+            title: 'Хорошие новости',
+            subMenus: [
+                {
+                    id: 45,
+                    title: 'Хорошие новости'
+                }
+            ]
+        },
+        {
+            id: 46,
+            title: 'Новости Киева',
+            subMenus: [
+                {
+                    id: 46,
+                    title: 'Новости Киева'
+                }
+            ]
+        },
+        {
+            id: 47,
+            title: 'Картина дня',
+            subMenus: [
+                {
+                    id: 47,
+                    title: 'Картина дня'
                 }
             ]
         },
@@ -108,84 +325,266 @@ RAD.menuMapping = {
     ],
     ukr: [
         {
-            id: 1,
-            title: 'Щоу-бизнес',
+            id: 3,
+            title: 'Всі новини',
             subMenus: [
                 {
-                    id: 1,
-                    title: 'Щоу-бизнес'
-                },
-                {
-                    id: 5,
-                    title: 'новости культуры'
-                },
-                {
-                    id: 6,
-                    title: 'новости кино'
-                },
-                {
-                    id: 7,
-                    title: 'музыка'
+                    id: 3,
+                    title: 'Всі новини'
                 }
             ]
         },
         {
             id: 2,
-            title: 'Спорт',
+            title: 'Новини спорту',
             subMenus: [
                 {
                     id: 2,
-                    title: 'Sport'
+                    title: 'Новини спорту'
                 },
                 {
                     id: 8,
-                    title: 'Box'
+                    title: 'Бокс'
                 },
                 {
                     id: 9,
-                    title: 'Football'
+                    title: 'Футбол'
                 },
                 {
                     id: 10,
-                    title: 'Basket'
+                    title: 'Баскетбол'
+                },
+                {
+                    id: 13,
+                    title: 'Теніс'
+                },
+                {
+                    id: 14,
+                    title: 'Формула 1'
+                },
+                {
+                    id: 15,
+                    title: 'Хокей'
+                },
+                {
+                    id: 16,
+                    title: 'Шахмати'
                 }
             ]
         },
         {
-            id: 3,
-            title: 'Все новости',
+            id: 17,
+            title: 'Новини бізнесу',
             subMenus: [
                 {
-                    id: 3,
-                    title: 'Все новости'
+                    id: 17,
+                    title: 'Новини бізнесу'
+                },
+                {
+                    id: 18,
+                    title: 'Новини компаній'
+                },
+                {
+                    id: 19,
+                    title: 'Новини економіки'
+                },
+                {
+                    id: 20,
+                    title: 'Новини фінансів'
+                },
+                {
+                    id: 21,
+                    title: 'ЗМІ та реклама'
+                },
+                {
+                    id: 22,
+                    title: 'Авто новини'
+                },
+                {
+                    id: 23,
+                    title: 'Індекси'
+                },
+                {
+                    id: 24,
+                    title: 'Фондовий ринок'
+                },
+                {
+                    id: 25,
+                    title: 'Інтернет та IT'
+                },
+                {
+                    id: 26,
+                    title: 'Нерухомість'
+                },
+                {
+                    id: 27,
+                    title: 'Кар\'єра'
+                }
+            ]
+        },
+        {
+            id: 1,
+            title: 'Шоу-бізнес',
+            subMenus: [
+                {
+                    id: 1,
+                    title: 'Шоу-бізнес'
+                },
+                {
+                    id: 5,
+                    title: 'Новини культури'
+                },
+                {
+                    id: 6,
+                    title: 'Новини кіно'
+                },
+                {
+                    id: 7,
+                    title: 'Музика'
                 }
             ]
         },
         {
             id: 4,
-            title: 'Новости Украины',
+            title: 'Новини України',
             subMenus: [
                 {
                     id: 4,
-                    title: 'Новости Украины'
+                    title: 'Новини України'
                 },
                 {
                     id: 11,
-                    title: 'События в Украине'
+                    title: 'Події в Україні'
                 },
                 {
                     id: 12,
-                    title: 'Новости политики'
+                    title: 'Новини політики'
+                }
+            ]
+        },
+        {
+            id: 28,
+            title: 'Наука та медицина',
+            subMenus: [
+                {
+                    id: 28,
+                    title: 'Наука та медицина'
+                },
+                {
+                    id: 29,
+                    title: 'Технологіх'
+                },
+                {
+                    id: 30,
+                    title: 'Новини науки'
+                },
+                {
+                    id: 31,
+                    title: 'Новини космосу'
+                }
+            ]
+        },
+        {
+            id: 32,
+            title: 'Новини світу',
+            subMenus: [
+                {
+                    id: 32,
+                    title: 'Новини світу'
+                },
+                {
+                    id: 33,
+                    title: 'Світ про нас'
+                },
+                {
+                    id: 34,
+                    title: 'Новини Росії'
+                }
+            ]
+        },
+        {
+            id: 35,
+            title: 'Lifestyle',
+            subMenus: [
+                {
+                    id: 35,
+                    title: 'Lifestyle'
+                },
+                {
+                    id: 36,
+                    title: 'Афіша заходів'
+                },
+                {
+                    id: 37,
+                    title: 'Новини моди'
+                },
+                {
+                    id: 38,
+                    title: 'Новини туризму'
+                },
+                {
+                    id: 39,
+                    title: 'Їжа та напої'
+                },
+                {
+                    id: 40,
+                    title: 'Автомобілі'
+                },
+                {
+                    id: 41,
+                    title: 'Гаджети'
+                },
+                {
+                    id: 42,
+                    title: 'Інтер\'єри'
+                },
+                {
+                    id: 43,
+                    title: 'Здоров\'я'
+                },
+                {
+                    id: 44,
+                    title: 'Відеоігри'
+                }
+            ]
+        },
+        {
+            id: 45,
+            title: 'Добрі новини',
+            subMenus: [
+                {
+                    id: 45,
+                    title: 'Добрі новини'
+                }
+            ]
+        },
+        {
+            id: 46,
+            title: 'Новини Києва',
+            subMenus: [
+                {
+                    id: 46,
+                    title: 'Новини Києва'
+                }
+            ]
+        },
+        {
+            id: 47,
+            title: 'Картина дня',
+            subMenus: [
+                {
+                    id: 47,
+                    title: 'Картина дня'
                 }
             ]
         },
         {
             id: 1000,
-            title: 'Избранное',
+            title: 'Обране',
             subMenus: [
                 {
                     id: 1000,
-                    title: 'Избранное'
+                    title: 'Обранеф'
                 }
             ]
         }
@@ -247,7 +646,7 @@ RAD.model('MenuData', Backbone.Model.extend({
 }), false);
 RAD.model('Sidebar', Backbone.Collection.extend({
     model: RAD.models.MenuData,
-    arr: [1,2,3,4,1000],
+    arr: [3,2,17,1,428,32,35,45,46,47,1000],
     resetWithOrder: function(){
         this.reset();
         var sortArr = JSON.parse(window.localStorage.getItem('sidebarOptionsOrder')) || this.arr,
